a2e2b129f01bacac2b8ce5abd99c189c8cbf3e6a
Johjen <Johjen@DESKTOP-R9FEKKS>
2017-08-14 22:55:22 +0530

    commit 8

--- MedicalLaboratory/src/main/java/org/hospital/management/laboratory/dao/LoginServiceDaoImpl.java
+++ MedicalLaboratory/src/main/java/org/hospital/management/laboratory/dao/LoginServiceDaoImpl.java
@@ -2,18 +2,27 @@ package org.hospital.management.laboratory.dao;
 
 import org.hibernate.SessionFactory;
 import org.hospital.management.laboratory.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
+@EnableTransactionManagement 
+@Transactional @Repository
 public class LoginServiceDaoImpl implements LoginServiceDao {
+	
 	private SessionFactory sessionFactory;
-
+	
+	@Autowired
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
 
 	public User findByUsername(String username) {
-
-		return (User) this.sessionFactory.getCurrentSession().createQuery("from User where username = ?")
+		User user = (User) this.sessionFactory.getCurrentSession().createQuery("from User where username = ?")
 				.setParameter(0, username).list().get(0);
+		System.out.println(user.getUsername() + " " + user.getPassword());
+		return user;
 
 	}
 
